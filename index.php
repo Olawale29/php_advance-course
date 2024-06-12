73818d1a12139399cdf09765b158f3a476d813fb
Olawale29 <oyegunleemmanuel39@gmail.com>
2024-06-12 13:46:32 +0100

    construct and destruct commit

--- index.php
+++ index.php
@@ -3,45 +3,47 @@
         public $name;
         public $color;
 
-        //set method
-        public function set_name($name){
+        public function __construct($name, $color)
+        {
             $this->name = $name;
+            $this->color = $color;
+            
         }
+     
         //get method
         public function get_name(){
             return $this->name;
         }
-
-        public function set_color($color){
-            $this->color = $color;
-        }
         public function get_color(){
             return $this->color;
         }
+        public function __destruct()
+        {
+            echo "The fruit is {$this->name} and the color is {$this->color}";
+            echo"<br/>";
+        }
     }
 
-    $apple = new Fruits();
-    $apple->set_name("apple");
-    $apple->set_color("blue");
+    $apple = new Fruits("apple 2", "green");
+ 
 
-    $bananna = new Fruits();
-    $bananna->set_name("banana");
-    $bananna->set_color("green");
+    $bananna = new Fruits("banana", "blue");
+
 
     
     //result
     // echo $bananna->get_name();
-    echo $bananna->name;
-    echo "<br/>";
+    // echo $bananna->name;
+    // echo "<br/>";
     // echo $apple->get_name();
-    echo $apple->name;
-    echo "<br/>";
-    echo "<br/>";
+    // echo $apple->name;
+    // echo "<br/>";
+    // echo "<br/>";
     // echo $bananna->get_color();
-    echo $bananna->color;
-    echo "<br/>";
+    // echo $bananna->color;
+    // echo "<br/>";
     // echo $apple->get_color();
-    echo $apple->color;
+    // echo $apple->color;
     
     
 ?>
