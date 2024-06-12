8cbde9a3248af7d154e899006683b7f7e0577ce3
Olawale29 <oyegunleemmanuel39@gmail.com>
2024-06-12 16:36:40 +0100

    constant keyword commit

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
     class Fruits{
         public $name;
         public $color;
+        public $weight;
+
+        const LEAVING_MESSAGE = "Thank you for visting our fruit stand";
 
         public function __construct($name, $color)
         {
@@ -11,25 +14,43 @@
         }
         public function intro(){
             echo "This fruit is {$this->name} and the color is {$this->color}";
-            echo "<br/>";
         }
         
 
     }
 
     class Strawberry extends Fruits{
+
+        public function __construct($name, $color, $weight)
+        {
+            $this->name= $name;
+            $this->color = $color;
+            $this->weight = $weight;
+            
+        }
+
+        public function intro(){
+            echo "This fruit is {$this->name} and the color is {$this->color}, and this weight is {$this->weight}";
+            
+        }
         public function message(){
             echo "Am I a fruit or berry";
         }
         public function addition($num1, $num2){
             echo $num1 + $num2;
         }
+        public function byebye(){
+            echo self::LEAVING_MESSAGE;
+        }
         
     }
-    $strawberry = new Strawberry("strawberry", "red");
+    $strawberry = new Strawberry("strawberry", "red" , "30kg");
+    // $strawberry->intro();
+    $strawberry->byebye();
     // $strawberry->addition(5,10);
-    echo $strawberry->name;
-    echo $strawberry->color;
+    // echo $strawberry->name;
+    // echo $strawberry->color;
+    
 
     
     
